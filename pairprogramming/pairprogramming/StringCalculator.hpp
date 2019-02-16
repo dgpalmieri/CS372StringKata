7648d5385c49a3a4b50462ae856d0d1149a954ba
jaarhus <jonathan.aarhus@gmail.com>
2019-02-15 15:39:24 -0900

    added functions for testing string calculator

---
+++ pairprogramming/pairprogramming/StringCalculator.hpp
@@ -0,0 +1,4 @@
+#include"catch.hpp"
+#include<string>
+
+int StringCalculator(std::string myString);
---
+++ pairprogramming/pairprogramming/test.cpp
@@ -0,0 +1,47 @@
+#include"catch.hpp"
+#include<string>
+using std::string
+
+TEST_CASE("String Calculator Tests") {
+
+	SECTION("Empty Sring")
+	{
+
+	}
+
+	SECTION("Number")
+	{
+
+	}
+
+	SECTION("Number Comma Number") {
+
+	}
+
+	SECTION("Number Newline Number") {
+
+	}
+
+	SECTIN("Number Delimination Number Delimination Number"){
+	}
+
+	SECTION("Negative Number") {
+	
+	}
+
+	SECTION("Number Over a Thousand") {
+
+	}
+
+	SECTION("Single Char Custom Deliminator") {
+
+	}
+
+	SECTION("Multi Char Custom Deliminator") {
+	
+	}
+
+	SECTION("Multiple Custom Deliminators"){
+	
+	}
+}
