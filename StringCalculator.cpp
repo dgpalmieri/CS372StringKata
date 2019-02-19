4ab17bf00f702b43ab50a5029b1f95d8093b2ccc
Dylan Palmieri <dgpalmieri@gmail.com>
2019-02-19 09:44:42 -0900

    Write tests for the 3_#s section (Compiles)

--- StringCalculator.cpp
+++ StringCalculator.cpp
@@ -8,20 +8,35 @@ double StringCalculator(std::string myString)
 		return 0.0;
 
 	bool twoNumbersPresent = false;
+	bool threeNumbersPresent = false;
 	std::string secondNumber;
+	std::string thirdNumber;
 	for (auto z : myString){
-		if (twoNumbersPresent)
+
+		if (twoNumbersPresent && !threeNumbersPresent)
 			secondNumber.push_back(z);
+		if (threeNumbersPresent)
+		    thirdNumber.push_back(z);
+
 	    if (z == ',' || z == '\n')
-	        twoNumbersPresent = true;
+            if (twoNumbersPresent)
+                threeNumbersPresent = true;
+	        if (!twoNumbersPresent)
+	            twoNumbersPresent = true;
+
 	}
 
-	if (twoNumbersPresent) {
+	if (twoNumbersPresent && !threeNumbersPresent) {
 		for (size_t i = 0; i < secondNumber.size() + 1; ++i) {
 			myString.pop_back();
 		}
-
 		return std::stod(myString) + std::stod(secondNumber);
+	}
+	else if (twoNumbersPresent && threeNumbersPresent) {
+        for (int i = 0; i < secondNumber.size() + thirdNumber.size(); ++i) {
+            myString.pop_back();
+        }
+        return std::stod(myString) + std::stod(secondNumber) + std::stod(thirdNumber);
 	}
 	else
 	    return std::stod(myString);
