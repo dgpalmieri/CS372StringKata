b07f9f53926735eb3960c23fb2aea9931de5d23c
Dylan Palmieri <dgpalmieri@gmail.com>
2019-02-19 10:42:59 -0900

    Refactor to better accommodate tests for section: -# (Compiles)

--- StringCalculator.cpp
+++ StringCalculator.cpp
@@ -9,14 +9,14 @@ double StringCalculator(std::string myString)
 
 	bool twoNumbersPresent = false;
 	bool threeNumbersPresent = false;
-	std::string secondNumber;
-	std::string thirdNumber;
+	std::string secondNumber_String;
+	std::string thirdNumber_String;
 	for (auto z : myString){
 
 		if (twoNumbersPresent && !threeNumbersPresent)
-			secondNumber.push_back(z);
+			secondNumber_String.push_back(z);
 		if (threeNumbersPresent)
-		    thirdNumber.push_back(z);
+		    thirdNumber_String.push_back(z);
 
 	    if (z == ',' || z == '\n') {
             if (twoNumbersPresent)
@@ -26,18 +26,36 @@ double StringCalculator(std::string myString)
         }
 	}
 
-	if (twoNumbersPresent && !threeNumbersPresent) {
-		for (size_t i = 0; i < secondNumber.size() + 1; ++i) {
-			myString.pop_back();
-		}
-		return std::stod(myString) + std::stod(secondNumber);
-	}
-	else if (twoNumbersPresent && threeNumbersPresent) {
-        for (int i = 0; i < secondNumber.size() + thirdNumber.size(); ++i) {
-            myString.pop_back();
-        }
-        return std::stod(myString) + std::stod(secondNumber) + std::stod(thirdNumber);
-	}
-	else
-	    return std::stod(myString);
+	size_t size = 0;
+    if (twoNumbersPresent)   {size += secondNumber_String.size();}
+    if (threeNumbersPresent) {size += thirdNumber_String.size();}
+
+    for (int i = 0; i < size; ++i) {
+        myString.pop_back();
+    }
+
+    double firstNumber_Double  = std::stod(myString);
+    double secondNumber_Double = std::stod(secondNumber_String);
+    double thirdNumber_Double  = std::stod(thirdNumber_String);
+
+    if (!threeNumbersPresent) {thirdNumber_Double = 0;}
+    if (!twoNumbersPresent)   {secondNumber_Double = 0;}
+
+    return firstNumber_Double + secondNumber_Double + thirdNumber_Double;
+
+//	if (twoNumbersPresent && !threeNumbersPresent) {
+//		for (size_t i = 0; i < secondNumber_String.size() + 1; ++i) {
+//			myString.pop_back();
+//		}
+//		return std::stod(myString) + std::stod(secondNumber_String);
+//	}
+//	else if (twoNumbersPresent && threeNumbersPresent) {
+//        for (int i = 0; i < secondNumber_String.size() + thirdNumber_String.size(); ++i) {
+//            myString.pop_back();
+//        }
+//        return std::stod(myString) + std::stod(secondNumber_String) + std::stod(thirdNumber_String);
+//	}
+//	else
+//	    return std::stod(myString);
+
 }
