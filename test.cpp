49ee7a98c4832feb3bc5904e409c88bd056cfea8
Dylan Palmieri <dgpalmieri@gmail.com>
2019-02-18 17:13:31 -0900

    Add tests for a single number to test.cpp

--- test.cpp
+++ test.cpp
@@ -13,7 +13,9 @@ TEST_CASE("String Calculator Tests") {
 	SECTION("Single number returns itself")
 	{
 		REQUIRE(StringCalculator("0") == 0);
-
+		REQUIRE(StringCalculator("1") == 1);
+		REQUIRE(StringCalculator("3.14") == 3.14);
+		REQUIRE(StringCalculator("10") == 10);
 	}
 
 	SECTION("Number Comma Number") {
