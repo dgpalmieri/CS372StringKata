91fa2592269f97b31fd1ff8e5589ac53707ab1b8
Dylan Palmieri <dgpalmieri@gmail.com>
2019-02-18 17:47:41 -0900

    Write initial code to pass #,# section (Compiles)

--- StringCalculator.cpp
+++ StringCalculator.cpp
@@ -4,8 +4,24 @@
 
 double StringCalculator(std::string myString)
 {
-	if (myString == "")
+	if (myString.empty())
 		return 0.0;
 
-	return std::stod(myString);
+	bool twoNumbersPresent = false;
+	std::string secondNumber;
+	for (auto z : myString){
+	    if (z == ',')
+	        twoNumbersPresent = true;
+	    if (twoNumbersPresent)
+	        secondNumber.push_back(z);
+	}
+
+    for (size_t i = 0; i < secondNumber.size(); ++i) {
+        myString.pop_back();
+    }
+
+	if (twoNumbersPresent)
+	    return std::stod(myString) + std::stod(secondNumber);
+	else
+	    return std::stod(myString);
 }
