2cd624dc5273e3e03d5a08f4b69061749f4b8116
Dylan Palmieri <dgpalmieri@gmail.com>
2019-02-18 17:56:36 -0900

    Pass all tests

--- StringCalculator.cpp
+++ StringCalculator.cpp
@@ -10,18 +10,19 @@ double StringCalculator(std::string myString)
 	bool twoNumbersPresent = false;
 	std::string secondNumber;
 	for (auto z : myString){
+		if (twoNumbersPresent)
+			secondNumber.push_back(z);
 	    if (z == ',')
 	        twoNumbersPresent = true;
-	    if (twoNumbersPresent)
-	        secondNumber.push_back(z);
 	}
 
-    for (size_t i = 0; i < secondNumber.size(); ++i) {
-        myString.pop_back();
-    }
+	if (twoNumbersPresent) {
+		for (size_t i = 0; i < secondNumber.size() + 1; ++i) {
+			myString.pop_back();
+		}
 
-	if (twoNumbersPresent)
-	    return std::stod(myString) + std::stod(secondNumber);
+		return std::stod(myString) + std::stod(secondNumber);
+	}
 	else
 	    return std::stod(myString);
 }
